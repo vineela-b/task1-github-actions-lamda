3014533b16e694985148aeb85a9eecf2da5d57ec
vineela-b <124869355+vineela-b@users.noreply.github.com>
2023-02-14 15:12:11 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
-# This program prints about the main page, world!
 
-print('Hello main page!')
+import json
+
+print('Hello, World!')
+
+def lambda_handler(event, context):
+    print('Testing artifacts!')
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
